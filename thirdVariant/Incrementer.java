d88acd65f44e89cdf349df0d70d366b2f4640b7b
Igor Mokritsky <igormokritsky@gmail.com>
2020-09-09 11:17:34 +0300

    AboutThreads

--- thirdVariant/Incrementer.java
+++ thirdVariant/Incrementer.java
@@ -4,39 +4,29 @@ import java.util.concurrent.BlockingQueue;
 
 public class Incrementer implements Runnable {
 
-
     private final BlockingQueue<Integer> incomingQueue;
     private final BlockingQueue<Integer> outcomingQueue;
 
-    static volatile int i = 0;
-    static boolean stop = false;
-
-    public Incrementer(BlockingQueue<Integer> incomingQueue,
-                       BlockingQueue<Integer> outcomingQueue) {
+    public Incrementer(BlockingQueue<Integer> incomingQueue, BlockingQueue<Integer> outcomingQueue) {
         this.incomingQueue = incomingQueue;
         this.outcomingQueue = outcomingQueue;
     }
-
 
     @Override
     public void run() {
         while (true) {
             try {
-                System.out.println(Thread.currentThread().getName() + " took " + i);
-                if (stop) {
-                    outcomingQueue.put(incomingQueue.take());
+                Integer incrementer = incomingQueue.take();
+                System.out.println(Thread.currentThread().getName() + " took " + incrementer);
+                if (incrementer == 100) {
+                    outcomingQueue.put(incrementer);
                     break;
-                } else if (i != 100) {
-                    i++;
                 } else {
-                    stop = true;
+                    outcomingQueue.put(incrementer + 1);
                 }
-                System.out.println(Thread.currentThread().getName() + " sent " + i);
-                outcomingQueue.put(incomingQueue.take());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-
     }
 }
--- thirdVariant/Main.java
+++ thirdVariant/Main.java
@@ -5,11 +5,11 @@ import java.util.concurrent.LinkedTransferQueue;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Integer i = 0;
+
         BlockingQueue<Integer> t1t2 = new LinkedTransferQueue<>();
         BlockingQueue<Integer> t2t3 = new LinkedTransferQueue<>();
         BlockingQueue<Integer> t3t1 = new LinkedTransferQueue<>();
-        t1t2.put(i);
+        t1t2.put(0);
         new Thread(new Incrementer(t3t1, t1t2)).start();
         new Thread(new Incrementer(t1t2, t2t3)).start();
         new Thread(new Incrementer(t2t3, t3t1)).start();
